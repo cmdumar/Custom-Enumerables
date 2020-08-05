837a870dc8efaf44ab6e2e75325d190e7f8e4e38
ershadul1 <ershadulrayhan@gmail.com>
2020-08-05 15:15:45 +0600

    Add test cases for my each when block is given

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -7,7 +7,8 @@ describe 'Custom Enumerable Methods' do
   let(:hash) { { a: 'A', b: 'B', c: 'C' } }
   let(:numbers) { [1, 2.332, 66, 90.7] }
   let(:words) { %w[cat dog lion dolphin] }
-  let(:block) { proc { |i| i < 10 } }
+  let(:block) { proc { |i| i } }
+  let(:block_num) { proc { |i| i < 5 } }
 
   describe '#my_each' do
     context 'No Block Given: ' do
@@ -24,5 +25,26 @@ describe 'Custom Enumerable Methods' do
       end
     end
  
+    context 'Block Given: ' do
+      it 'when applied on an array' do
+        expect(array.my_each(&block_num)).to eql(array.each(&block_num))
+      end
+
+      it 'when applied on a range' do
+        expect(range.my_each(&block_num)).to eql(range.each(&block_num))
+      end
+
+      it 'when applied on an hash' do
+        expect(hash.my_each(&block)).to eql(hash.each(&block))
+      end
+
+      it 'when applied on a numbers array' do
+        expect(numbers.my_each(&block_num)).to eql(numbers.each(&block_num))
+      end
+
+      it 'when applied on a words array' do
+        expect(words.my_each(&block)).to eql(words.each(&block))
+      end
+    end
   end
 end
